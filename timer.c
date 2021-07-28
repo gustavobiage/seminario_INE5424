54b86b2038571c6fd576e5f475f31d62ce9b3990
Gilson <gilson.t.magro@gmail.com>
2021-07-27 21:29:27 -0300

    Melhorias no arquivo timer.c

--- timer.c
+++ timer.c
@@ -6,6 +6,8 @@ extern void disable_irq(void);
 extern void io_halt(void);
 extern void schedule();
 
+unsigned int cntfrq = 0;
+
 void routing_core0cntv_to_core0irq(void)
 {
     (*(volatile unsigned int*) CORE0_TIMER_IRQCNTL) = 0x08;
@@ -18,39 +20,37 @@ unsigned int read_core0timer_pending(void)
     return tmp;
 }
 
-static unsigned int cntfrq = 0;
-
 void enable_cntv(void)
 {
     unsigned int cntv_ctl;
     cntv_ctl = 1;
-	asm volatile ("mcr p15, 0, %0, c14, c3, 1" :: "r"(cntv_ctl) ); // write CNTV_CTL
+    asm volatile ("mcr p15, 0, %0, c14, c3, 1" :: "r"(cntv_ctl)); // write CNTV_CTL
 }
 
 void disable_cntv(void)
 {
     unsigned int cntv_ctl;
     cntv_ctl = 0;
-	asm volatile ("mcr p15, 0, %0, c14, c3, 1" :: "r"(cntv_ctl) ); // write CNTV_CTL
+    asm volatile ("mcr p15, 0, %0, c14, c3, 1" :: "r"(cntv_ctl)); // write CNTV_CTL
 }
 
 unsigned int read_cntv_tval(void)
 {
     unsigned int val;
-	asm volatile ("mrc p15, 0, %0, c14, c3, 0" : "=r"(val) );
+    asm volatile ("mrc p15, 0, %0, c14, c3, 0" : "=r"(val) );
     return val;
 }
 
 inline void write_cntv_tval(unsigned int val)
 {
-	asm volatile ("mcr p15, 0, %0, c14, c3, 0" :: "r"(val) );
+    asm volatile ("mcr p15, 0, %0, c14, c3, 0" :: "r"(val) );
     return;
 }
 
 unsigned int read_cntfrq(void)
 {
     unsigned int val;
-	asm volatile ("mrc p15, 0, %0, c14, c0, 0" : "=r"(val) );
+    asm volatile ("mrc p15, 0, %0, c14, c0, 0" : "=r"(val) );
     return val;
 }
 
